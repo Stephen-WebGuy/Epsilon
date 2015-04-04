e80e777b56985fde3c755397f517b54077c6addf
timpur <timothy@purchas.com>
2015-04-04 13:15:07 +1100

    Changed Drag and Drop to be Dynamic

--- contents/js/epsilon.js
+++ contents/js/epsilon.js
@@ -1,1 +1,1 @@
-﻿
+﻿// This is whare common functions should go
--- contents/js/epsilonAngular.js
+++ contents/js/epsilonAngular.js
@@ -3,15 +3,21 @@
 /// <reference path="angular-dragdrop.min.js" />
 /// <reference path="angular.min.js" />
 /// <reference path="jquery-2.1.3.min.js" />
+
+// This is where angular functions and dependent functions should go unless common.
+
+
 var app = angular.module('epsilon', ['ngDragDrop']);
 
 app.run(function ($rootScope) {
     // Add / Change the root for shared objects;
-    $rootScope.rootimages = [{}, {}, {}];
+
+    // This is where image locations and id are located and will be coppeid for use in controllers 
+    $rootScope.rootimages = [{ ID: 1, src: "" }, { ID: 2, src: "" }, { ID: 3, src: "" }];
 });
 
 app.controller("main", function ($scope, $rootScope) {
-    
+
 });
 
 app.controller("content", function ($scope, $rootScope) {
@@ -19,62 +25,70 @@ app.controller("content", function ($scope, $rootScope) {
 });
 
 app.controller("staticImages", function ($scope, $rootScope) {
+    // This is the controler to control the Static Images
     $scope.images = createImageFromRoot($rootScope.rootimages);
-    
+
 });
-
 app.controller("dragableImages", function ($scope, $rootScope) {
+    // This is the controller to control the dragable images
+    // This is where the images are head. Duplicated from root images.
     $scope.images = createImageFromRoot($rootScope.rootimages);
-    $scope.img_1 = $scope.images[0];
-    $scope.img_2 = $scope.images[1];
-    $scope.img_3 = $scope.images[2];
-    $scope.tempLocation = {};
-    $scope.lastDroped = null;
-    $scope.onStart = function (item, data) {
-
-    };
+    // push another object that is empty for the blank space
+    $scope.images.push({});
+    // On drop event.
+    // Finds the droped image and the target location and logs those details.
     $scope.onDrop = function (event, data) {
         var itemDroped = data.draggable[0];
+        // get the index of the image that was droped
         var index = $(itemDroped).attr("data-index");
         if (!index == "" || !index) {
+            // turn index into number
             index = Number(index);
+            // set "itemDroped" to actual object in array
             itemDroped = $scope.images[index];
+
+            // get from location which is the current location
             var From = itemDroped.currentLocation;
-            var To = $(event.target).attr("ng-model");
-            if (To == "img_1") To = "Location 1"
-            else if (To == "img_2") To = "Location 2"
-            else if (To == "img_3") To = "Location 3"
-            else if (To == "tempLocation") To = "Temp Location"
+            // get the new location from the target. add one because is index and locations start from 1 to n
+            var To = Number($(event.target).attr("data-index")) + 1;
+
+            // create history object and add to images history array
             itemDroped.history.push(createImageMovement(From, To));
+            // update the current location to new location
             itemDroped.currentLocation = To;
 
+            // disable the new location for dropable
             $(event.target).droppable("disable");
-            $(data.draggable[0]).parent().parent().droppable("enable");
-        }       
-        
+            // enable the old dropable location as it is now a vacant spot 
+            $(data.draggable[0]).parent().droppable("enable");
+        }
+
     };
 });
 
 
 function createImageFromRoot(rootImages) {
+    // This function coppies config from root images to new local instances of an image.
+    // Also add other parameters to this local image.
     var images = [];
     for (var i = 0; i < 3; i++) {
         var image = {};
         image.isImage = true;
+        image.ID = rootImages[i].ID
         image.scr = rootImages[i].scr;
         image.index = i;
-        image.startLocation = "Location " + (i + 1);
+        image.startLocation = i + 1;
         image.currentLocation = image.startLocation;
         image.history = [];
-        image.history.push(createImageMovement("", image.currentLocation));
         images.push(image);
     }
     return images;
 }
 
-function createImageMovement(from,to) {
+function createImageMovement(from, to) {
+    // this function creates a hitory object
     var item = {};
-    item.from = from;
-    item.to = to;
+    item.from = "Location " + from;
+    item.to = "Location " + to;
     return item
 }
