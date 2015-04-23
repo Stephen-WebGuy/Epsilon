a5671560372171a91eae5ccf794d97ea52893e59
timpur <timothy@purchas.com>
2015-04-23 20:57:30 +1000

    Tied Together Pages and Nav

---
+++ BasicWeb/contents/js/results.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="bootstrap.min.js" />
+/// <reference path="jquery-ui.min.js" />
+/// <reference path="angular-dragdrop.min.js" />
+/// <reference path="angular.min.js" />
+/// <reference path="jquery-2.1.3.min.js" />
+/// <reference path="epsilon.js" />
+
+var app = angular.module('results', []);
+var theSession;
+
+app.run(function ($rootScope) {
+    session.Load();
+});
+
+app.controller("mainController", function ($scope, $rootScope) {
+    theSession = session.Session;
+    theSession.levels
+
+    $scope.session = theSession;
+});
+
